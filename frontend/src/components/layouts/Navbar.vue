8c62b2ddbdd9fa5fb41d25d6b3c15ab5bd1376c2
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-29 13:26:08 +0300

    32. Display the currently logged in user & Logout

--- frontend/src/components/layouts/Navbar.vue
+++ frontend/src/components/layouts/Navbar.vue
@@ -27,16 +27,47 @@
             <i class="bi bi-house-door-fill"></i> Home
           </router-link>
         </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/register">
-            <i class="bi bi-person-add"></i> Register
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/login">
-            <i class="bi bi-box-arrow-right"></i> Login
-          </router-link>
-        </li>
+        <ul class="navbar-nav" v-if="!authStore.isLoggedIn">
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                aria-current="page"
+                to="/register"
+              >
+                <i class="bi bi-person-add"></i> Register
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                aria-current="page"
+                to="/login"
+              >
+                <i class="bi bi-box-arrow-right"></i> Login
+              </router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav" v-else>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                aria-current="page"
+                to="/profile"
+              >
+                <i class="bi bi-person-check-fill"></i> {{ authStore.user.name }}
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                @click="userLogout"
+                aria-current="page"
+                to="#"
+              >
+                <i class="bi bi-box-arrow-left"></i> Logout
+              </router-link>
+            </li>
+          </ul>
         <li class="nav-item">
           <router-link class="nav-link" to="/cart">
             <i class="bi bi-cart-plus"></i> Cart ({{
@@ -50,11 +81,72 @@
 </template>
 
 <script setup>
+import axios from "axios";
+import { useAuthStore } from "../../../stores/useAuthStore";
 import { useCartStore } from "../../../stores/useCartStore";
+import { BASE_URL, headersConfig } from "@/helpers/config";
+import { useToast } from "vue-toastification";
+import { useRouter } from "vue-router";
+import { onMounted } from "vue";
 
 // define to here cart store
-
 const cartStore = useCartStore();
+
+// define the toast 
+
+const toast = useToast()
+
+// define the router for redirect
+
+const router = useRouter()
+
+// Define the check user if exist or not
+
+const authStore = useAuthStore()
+
+// logout function
+
+const userLogout = async() =>{
+  try {
+    const response = await axios.post(`${BASE_URL}/user/logout`,null,
+    headersConfig(authStore.access_token))
+    authStore.clearAuthData()
+    toast.success(response.data.message,{
+      timeout:2000
+    })
+    router.push('/login')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+// fetch the currently logged in user
+// check if the token is still valid
+
+const fetchCurrentUser = async() =>{
+  try {
+    const response = await axios.get(`${BASE_URL}/user`,
+    headersConfig(authStore.access_token))
+    authStore.setIsLoggedIn()
+    authStore.setUser(response.data.user)
+    authStore.setToken(response.data.access_token)
+  } catch (error) {
+    if(error.response.status === 401){
+      authStore.clearAuthData()
+    }
+    
+    console.log(error)
+
+  }
+}
+
+
+// once the component is loaded we get the currently logged in user 
+
+onMounted(()=>authStore.isLoggedIn && fetchCurrentUser())
+
+
 </script>
 
 <style scoped>
--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,5 +1,14 @@
 export const BASE_URL = 'http://127.0.0.1:8000/api'
 
+export const headersConfig = (token,contentType) => {
+  const config = {
+    headers : {
+      "Authorization":`Bearer ${token}`,
+      "Content-type":contentType || "application/json"
+    }
+  }
+  return config
+}
 
 // create a unique ref for each product
 export const makeUniqueId = (length) => {
