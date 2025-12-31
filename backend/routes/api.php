fefa628288f4d3876d286d01d4c0a140cf2adaa0
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-31 17:06:15 +0300

    39.Order API Controller & Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CouponController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Resources\UserResource;
@@ -20,6 +21,10 @@ Route::middleware('auth:sanctum')->group(function(){
     // Coupon Route 
 
     Route::post('apply/coupon',[CouponController::class,'applyCoupon']);
+
+    // Order routes
+    Route::post('store/order',[OrderController::class,'storeUserOrders']);
+    Route::post('pay/order',[OrderController::class,'payOrdersByStripe']);
 });
 
 
